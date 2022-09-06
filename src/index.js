f441cf7a7f2b7b4c9d0bdb20d55ae235dacd9dd6
MargaritaKhodzka <>
2022-09-06 17:50:00 -0400

    players take turns

--- src/index.js
+++ src/index.js
@@ -2,35 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-// React.Component subclass
-class Square extends React.Component {
-    // render method returns a description of what you want to see on the screen
-    render() {
-      return (
-        // display the current state’s value when clicked
-        <button 
-            className="square" 
-            onClick={() => this.props.onClick({value: 'X'})}
-        >
-          {this.props.value}
+// function components are a simpler way to write components that only contain a render method and don’t have their own state
+function Square(props) {
+    return (
+        <button className='square' onClick={props.onClick}>
+            {props.value}
         </button>
-      );
-    }
+    );
 }
-  
+
+// React.Component subclass
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         // call .slice() to create a copy of the squares array to modify instead of modifying the existing array
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -45,7 +43,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
